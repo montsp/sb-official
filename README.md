d55653c5bba78c93943c219d469e68038cba588c
montsp <konbuda4@icloud.com>
2021-10-14 18:16:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-prcuq6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-prcuq6)
---
+++ index.js
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>こんにちは Hello!</h1>
+<div>
+<p>I'm montsp. 私はmontspです。</p>
+<p>I'm Japanese日本人です。</p>
+<p>Click here for the web.</p>
+<p><a href="https://scratch.mit.edu/users/montowakami/">Scratch</a></p>
+<p><a href="https://stackblitz.com/@montsp">StackBlitz</a></p>
+<p><a href="https://codesandbox.io/u/montsp">Codesandbox</a></p>
+</div>`;
